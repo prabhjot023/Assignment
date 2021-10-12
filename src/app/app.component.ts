27233dd24ae4d8c9da9203f46670ba01827ba002
jaspreet-randhawa <kjaspreet011@gmail.com>
2021-10-12 12:09:39 +0530

    assignmnet done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatTableDataSource, } from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Assignment1';
+export class AppComponent implements OnInit{
+  
+  validationForm:any ;
+  displayedColumns: any;
+  dataSource:any;
+  count: any=0;
+  savedData: any=[];
+  submitted: boolean =false;
+  constructor(public fb:FormBuilder)
+  {
+    
+  }
+
+  ngOnInit()
+  {
+    this.initForm();
+   this.displayedColumns = ['position', 'name', 'email', 'phone_no','gender'];
+
+  }
+
+  initForm()
+  {
+    this.validationForm=this.fb.group({
+      'name':['',Validators.required],
+      'email':['',[Validators.email,Validators.required]],
+      'phone_no':['',Validators.required],
+      'gender':['',Validators.required]
+    })
+  }
+
+  showTable()
+  {
+    if(this.validationForm.valid)
+    {
+      this.submitted=true;
+     this.count++;
+      let data={
+        'position':this.count,
+        'name':this.validationForm.controls['name'].value,
+        'email':this.validationForm.controls['email'].value,
+        'phone_no':this.validationForm.controls['phone_no'].value,
+        'gender':this.validationForm.controls['gender'].value
+        
+      }
+      console.log(data);
+      this.savedData.push(data);
+      this.dataSource=new MatTableDataSource(this.savedData);
+    }
+  }
+
+  AlphabetOnly(event:any){
+    let pattAlpha = /^([a-zA-Z ])*$/;
+    let resultAlpha = pattAlpha.test(event.key);
+    return resultAlpha;
+     }
+  
+     NumberOnly(event:any){
+      let pattAlpha = /^([0-9])*$/;
+      let resultAlpha = pattAlpha.test(event.key);
+      return resultAlpha;
+       }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
